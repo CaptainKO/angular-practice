4e81a455013be5d4d690d18475d1f059d1379a29
CaptainLee <17521098@gm.uit.edu.vn>
2018-12-26 13:36:14 +0700

    Update login and signin component

--- src/app/modules/user/pages/login/login.component.ts
+++ src/app/modules/user/pages/login/login.component.ts
@@ -1,15 +1,59 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+
+import { UserStoreService } from "@core/services/user-store.service";
+import { UserService } from '@core/services/user.service';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+  message= "";
+  constructor(
+    private userService: UserService,
+    private userStoreService: UserStoreService,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.loadForm();
+  }
 
-  constructor() { }
+  loadForm() {
+    this.loginForm = this.fb.group({
+      username: [null, [
+        Validators.required
+      ]],
+      password: [null, [
+        Validators.required
+      ]]
+    });
+  }
+
+  get username() {
+    return this.loginForm.get('username');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
 
   ngOnInit() {
   }
 
+  login() {
+    this.userService.login(this.username.value, this.password.value)
+      .subscribe(x => {
+        this.message = x.msg;
+        this.userStoreService.token = x.token
+        this.router.navigate(['user', 'profile'])
+      }, err => {
+        this.message = err.error.msg
+      });
+  }
 }
--- src/app/modules/user/pages/signin/signin.component.ts
+++ src/app/modules/user/pages/signin/signin.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from "@angular/router";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
 
+
+import { UserService } from '@app/core/services/user.service';
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -7,10 +11,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['../login/login.component.scss']
 })
 export class SigninComponent implements OnInit {
+  signinForm: FormGroup;
+  message: string;
+  constructor(
+    private fb: FormBuilder,
+    private userService: UserService,
+    private router: Router
+  ) {
+    this.signinForm = this.fb.group({
+      username: ['', [
+        Validators.required,
+        Validators.min(5),
+        Validators.maxLength(25)
+      ]],
+      password: ['',
+        [
+          Validators.required,
+          Validators.minLength(6)        ]
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email
+        ]
+      ]
+    });
+    this.signinForm.valueChanges.subscribe(console.log);
+  }
+  get username() {
+    return this.signinForm.get('username');
+  }
 
-  constructor() { }
+  get password() {
+    return this.signinForm.get('password');
+  }
 
+  get email() {
+    return this.signinForm.get('email');
+  }
   ngOnInit() {
+  }
+  register() {
+    console.log(this.username, this.password);
+    this.userService.register(this.username.value, this.password.value)
+      .subscribe((res) => {
+        this.message = res.msg;
+        this.router.navigateByUrl('user/login');
+      }, (err) => {
+        this.message = err.error.msg
+      });
   }
 
 }
