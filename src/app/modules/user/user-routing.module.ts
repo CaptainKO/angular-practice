bd6ab5d6c89eda6e7c7fa638738379baa3564823
CaptainLee <17521098@gm.uit.edu.vn>
2019-01-11 19:32:05 +0700

    add more paths to user-routing

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -6,22 +6,29 @@ import { AuthGuard } from "@core/guards/auth.guard";
 import { LoginComponent } from "./pages/login/login.component";
 import { SigninComponent } from "./pages/signin/signin.component";
 import { ProfileComponent } from "./pages/profile/profile.component";
+import { SharedModule } from '@shared/shared.module';
 
 
-const routes: Routes = [
-  { path: 'login', component: LoginComponent },
-  { path: 'signin', component: SigninComponent },
+const routerConfig: Routes = [
+  {path: '', redirectTo: 'profile', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent, data: {breadcrumb: 'Login'} },
+  { path: 'signin', component: SigninComponent,  data: {breadcrumb: 'Signin'} },
   {
     path: 'profile', children: [
       {
-        path: '', pathMatch: 'full', component: ProfileComponent
+        path: '',
+        pathMatch: 'full',
+        component: ProfileComponent,
+        data: {
+          breadcrumb: 'Profile'
+        }
       }
-    ], canActivate: [AuthGuard]
+    ], canActivate: [AuthGuard],data : {breadcrumb: 'Profile'}
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
+  imports: [RouterModule.forChild(routerConfig), SharedModule],
   exports: [RouterModule]
 })
 export class UserRoutingModule { }
